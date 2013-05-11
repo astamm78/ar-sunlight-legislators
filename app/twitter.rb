d94f137a669b1d581b15f9e95e4afcb680916da4
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-11 15:33:47 -0500

    twitter commit

--- app/twitter.rb
+++ app/twitter.rb
@@ -1,6 +1,7 @@
 require 'twitter'
 require 'json'
 require_relative '../db/config'
+require_relative 'models/politician'
 
 Twitter.configure do |config|
   config.consumer_key = 'eWtK22bsi8vTo1GWFcUlzQ'
@@ -9,4 +10,10 @@ Twitter.configure do |config|
   config.oauth_token_secret = 'iAg81ftpVtgtjhKF2LWXYchb4KqnETPJBpghYbYkGA'
 end
 
-Twitter.update('This DBC stuff rocks')
+politician = Politician.find_by_lastname('Durbin')
+
+Twitter.user_timeline(politician.twitter_id, :count => 10).each do |tw|
+  p tw.text
+  p tw.id
+  p tw.created_at
+end
